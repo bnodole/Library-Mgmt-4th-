35c977b075bac739154c63b9d0e5ab4272726064
bnodole <bnodole01@gmail.com>
2024-01-30 16:13:36 +0545

    .

--- admin/addform.php
+++ admin/addform.php
@@ -1,14 +1,23 @@
 <?php
     session_start();
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        //check if 'semester' and 'Add Book' are set
-        if (isset($_POST['sems']) && isset($_POST['adbook'])){
-            $sub = $_POST['adbook'];    //get the book name 
-            $semester = $_POST['sems']; //get semester from dropdown
+    $showad=$showdel=null;
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        // Check if 'semester' and 'Add Book' are set
+        if (isset($_POST['sems']) && isset($_POST['adbook'])) {
+            $sub = $_POST['adbook'];    // Get the book name 
+            $semester = $_POST['sems']; // Get semester from dropdown
+            $showad=1;
         }
+        // Check if 'semester' and 'Delete Book' are set
+        else if (isset($_POST['sems']) && isset($_POST['delbook'])) {
+            $sub = $_POST['delbook'];    // Get the book name 
+            $semester = $_POST['sems']; // Get semester from dropdown
+            $showdel = 1;
+        }
+    }
+    
         $_SESSION['sub']=$sub;
         $_SESSION['semester']=$semester;
-    }
     // if (isset($_SESSION['status'])){
     //     echo "<script>alert('Status');</script>";
     // }
@@ -21,9 +30,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Books</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form method="POST" action="addformprocess.php">
+    <?php if($showad === 1){   ?>
+    <form method="POST" action="addformprocess.php" id="addform">
         <h1>Add Books</h1>
         <label for="semester">Semester:</label>
         <input type="text" name="semester" id="semester" value="<?php echo $_SESSION['semester'];?>" readonly>
@@ -33,5 +44,18 @@
         <input type="text" name="bookname" id="bookname" required>
         <button type="submit" name="adb">Add Book</button>
     </form>
+    <?php } ?>
+    <?php if($showdel === 1){   ?>
+    <form method="POST" action="addformprocess.php" id="delform">
+        <h1>Remove Books</h1>   
+        <label for="semester">Semester:</label>
+        <input type="text" name="semester" id="semester" value="<?php echo $_SESSION['semester'];?>" readonly>
+        <label for="sub">Subject:</label>
+        <input type="text" name="subject" id="sub" value="<?php echo $_SESSION['sub'];?>" readonly>
+        <label for="bookname">Bookname:</label>
+        <input type="text" name="bookname" id="bookname" required>
+        <button type="submit" name="dlb">Add Book</button>
+    </form>
+    <?php } ?>
 </body>
 </html>
